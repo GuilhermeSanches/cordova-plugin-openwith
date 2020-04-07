099e57c9992715430f85f12091dfd463517438aa
Guilherme Sanches <guilherme@faci.ly>
2020-04-07 18:47:49 -0300

    change strings

--- src/ios/ShareExtension/ShareViewController.h
+++ src/ios/ShareExtension/ShareViewController.h
@@ -1,2 +1,2 @@
-#define SHAREEXT_GROUP_IDENTIFIER @"group.__BUNDLE_IDENTIFIER__"
-#define SHAREEXT_URL_SCHEME @"__URL_SCHEME__"
+#define SHAREEXT_GROUP_IDENTIFIER @"group.com.facily"
+#define SHAREEXT_URL_SCHEME @"cordovaopenwithfacily"
